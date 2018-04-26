e87896298dd6bac7cf5c6f052e3eda5eb5fa2c77
Atichat43 <boss_atln@outlook.com>
2018-04-26 16:22:57 +0700

    fix views.py, now it works find

--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from my_app.models import Post, Comment
+from django.utils import timezone
+from my_app.forms import PostForm, CommentForm
 
 from django.views.generic import (TemplateView,ListView,
                                   DetailView,CreateView,
                                   UpdateView,DeleteView)
 
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        # __lte: less than or equal to
+        # document search django queries
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
